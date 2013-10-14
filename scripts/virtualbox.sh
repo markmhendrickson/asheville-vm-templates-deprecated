e2c54d7725f069ca280f1822f3f98fec4c14c910
Jack Pearkes <jackpearkes@gmail.com>
2013-10-14 16:13:41 +0200

    api: fix virtualbox puppet by setting a hostname

--- scripts/virtualbox.sh
+++ scripts/virtualbox.sh
@@ -8,8 +8,7 @@ aptitude -y purge virtualbox-ose-guest-x11 virtualbox-ose-guest-dkms virtualbox-
 aptitude -y install dkms
 
 # Install the VirtualBox guest additions
-VBOX_VERSION=$(cat /home/asheville/.vbox_version)
-VBOX_ISO=VBoxGuestAdditions_$VBOX_VERSION.iso
+VBOX_ISO=VBoxGuestAdditions.iso
 mount -o loop $VBOX_ISO /mnt
 yes|sh /mnt/VBoxLinuxAdditions.run
 umount /mnt
